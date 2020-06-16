5deb6a9eaff89f752d2920c7723e7eb568a330ab
brandonktran <brandontran@berkeley.edu>
2020-06-16 22:40:45 +0000

    view average grade complete

--- components/app.js
+++ components/app.js
@@ -1,16 +1,23 @@
 var key = 'kdAPDHqi';
 
 class App {
-	constructor(gradeTable) {
+	constructor(gradeTable, pageHeader) {
 		this.handleGetGradesError = this.handleGetGradesError.bind(this);
 		this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
 		this.gradeTable = gradeTable;
+		this.pageHeader = pageHeader;
 	}
 	handleGetGradesError(error) {
 		console.error(error);
 	}
 	handleGetGradesSuccess(grades) {
 		this.gradeTable.updateGrades(grades);
+		var avg = 0;
+		for (var i = 0; i < grades.length; i++) {
+			avg += grades[i].grade;
+		}
+		avg = avg / grades.length;
+		this.pageHeader.updateAverage(avg);
 	}
 	getGrades() {
 		$.ajax({
